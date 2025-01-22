fe2c88ebc2d3be6123626eba0bbd65eb368245b7
mk-isos <mkisos7900@naver.com>
2025-01-22 11:01:25 +0900

    250122 feat : 회원가입 시간 조회

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -23,11 +23,21 @@ public class MemberService {
 
     /* 회원가입 */
     public long join(Member member){
-        //같은 이름 회원 x
-        validateDuplicateMember(member);
 
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try{
+            //같은 이름 회원 x
+            validateDuplicateMember(member);
+
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long duration = finish - start;
+            System.out.println("join = " + duration + "ms");
+        }
+
     }
 
     private void validateDuplicateMember(Member member) {
