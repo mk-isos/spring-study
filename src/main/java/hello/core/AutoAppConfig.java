cf82bc35e55c8edaa09fc8059695446427c482b9
mk-isos <mkisos7900@naver.com>
2025-02-06 17:01:43 +0900

    feat: set base package for component scan

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,9 +6,13 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        //basePackages = "hello.core",
+        // 이를 안붙혀도 @ComponetScan 이 붙은 설정 정보 클래스의 패키지가 시작 위치가 된다.
+
         //예제 유지를 위해 일단은 뺌. 안전하게.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Configuration.class})
 )
+
 public class AutoAppConfig {
 
 }
