d2bc85d75cf5eb6974cd470a4f55fe830f850d5c
mk-isos <mkisos7900@naver.com>
2025-02-06 13:51:49 +0900

    feat: ConfigurationSingletonTest

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -7,11 +7,17 @@ public class MemberServiceImpl implements MemberService {
         this.memberRepository = memberRepository;
     }
 
+    @Override
     public void join(Member member) {
         memberRepository.save(member);
     }
-
+    @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
+    }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
