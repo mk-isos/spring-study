3259851d83e8dc6f10e5314f006832252815d6f5
mk-isos <mkisos7900@naver.com>
2025-02-06 14:13:10 +0900

    feat: ConfigurationSingletonTest & byteCode manipulation

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -27,6 +27,15 @@ public class AppConfig {
         return new MemoryMemberRepository();
     }
 
+    //덕분에 싱글톤이 보장.
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        if (memoryMemberRepository가 이미 스프링 컨테이너에 등록되어 있으면?) { return 스프링 컨테이너에서 찾아서 반환;
+//        } else { //스프링 컨테이너에 없으면
+//            기존 로직을 호출해서 MemoryMemberRepository를 생성하고 스프링 컨테이너에 등록 return 반환
+//        }
+//    }
+
     @Bean
     public OrderService orderService() {
         System.out.println("call AppConfig.orderService");
