a11b0f466b9897125bd805fff938d93a4e2dda9d
mk-isos <mkisos7900@naver.com>
2025-02-07 13:22:14 +0900

    feat: Add @Primary annotation to resolve bean ambiguity

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -14,5 +17,13 @@ import org.springframework.context.annotation.FilterType;
 )
 
 public class AutoAppConfig {
+
+//    @Bean(name = "memoryMemberRepository")
+//
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+
+    //이 경우 수동 빈 등록이 우선권을 가진다. (수동 빈이 자동 빈을 오버라이딩 해버린다.)
 
 }
