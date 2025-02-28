0bd921f1d56de3080f673e768c5098730996ea85
mk-isos <mkisos7900@naver.com>
2025-02-28 13:16:08 +0900

    250228 feat: Implement FrontController V2 pattern with MyView abstraction for rendering views

--- springmvc/servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
+++ springmvc/servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.util.List;
 
 public class MemberListControllerV1 implements ControllerV1 {
+
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
