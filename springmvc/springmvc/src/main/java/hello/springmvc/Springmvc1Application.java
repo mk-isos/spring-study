4edf42c18afce51abdb6e25365e65b0ab7fc99eb
mk-isos <mkisos7900@naver.com>
2025-03-05 13:30:31 +0900

    250305 refactor: Rename Springmvc1Application to Springmvc

--- springmvc/springmvc/src/main/java/hello/springmvc/Springmvc1Application.java
+++ springmvc/springmvc/src/main/java/hello/springmvc/Springmvc1Application.java
@@ -1,4 +1,4 @@
-package hello.springmvc1;
+package hello.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- springmvc/springmvc/src/test/java/hello/springmvc/Springmvc1ApplicationTests.java
+++ springmvc/springmvc/src/test/java/hello/springmvc/Springmvc1ApplicationTests.java
@@ -1,4 +1,4 @@
-package hello.springmvc1;
+package hello.springmvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
