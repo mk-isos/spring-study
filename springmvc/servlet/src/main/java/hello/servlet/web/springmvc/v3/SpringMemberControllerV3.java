c0ca77538d21aabbdc37cc90c1c1d0d366f1386f
mk-isos <mkisos7900@naver.com>
2025-03-05 11:01:26 +0900

    250305 feat: Refactor Spring MVC V3 controller using @GetMapping and @PostMapping

--- springmvc/servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ springmvc/servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -4,8 +4,7 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
@@ -16,12 +15,14 @@ public class SpringMemberControllerV3 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/new-form")
+    //@RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping("/new-form")
     public String newForm() {
         return "new-form"; //논리이름
     }
 
-    @RequestMapping("/save")
+    //@RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save")
     public String save(
             @RequestParam("username") String username,
             @RequestParam("age") int age,
@@ -34,7 +35,8 @@ public class SpringMemberControllerV3 {
         return "save-result";
     }
 
-    @RequestMapping
+    //@RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String members(Model model) {
 
         List<Member> members = memberRepository.findAll();
