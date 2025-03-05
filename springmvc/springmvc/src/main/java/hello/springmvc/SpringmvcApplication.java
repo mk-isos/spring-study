df90dc12e88b21f632c622bb864cdd141db2652b
mk-isos <mkisos7900@naver.com>
2025-03-05 13:36:08 +0900

    Refactor package name from springmvc1 to springmvc

--- springmvc/springmvc/src/main/java/hello/springmvc/SpringmvcApplication.java
+++ springmvc/springmvc/src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springmvc1Application {
+public class SpringmvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springmvc1Application.class, args);
+		SpringApplication.run(SpringmvcApplication.class, args);
 	}
 
 }
--- springmvc/springmvc/src/test/java/hello/springmvc/SpringmvcApplicationTests.java
+++ springmvc/springmvc/src/test/java/hello/springmvc/SpringmvcApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Springmvc1ApplicationTests {
+class SpringmvcApplicationTests {
 
 	@Test
 	void contextLoads() {
