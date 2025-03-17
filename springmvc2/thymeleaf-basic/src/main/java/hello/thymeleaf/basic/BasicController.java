dd0218eb12fca4b4693e1799537cb2aa4068fb0c
mk-isos <mkisos7900@naver.com>
2025-03-17 16:03:18 +0900

    250317 feat: add Thymeleaf iteration and conditional rendering examples

--- springmvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ springmvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -103,6 +103,25 @@ public class BasicController {
         return "basic/attribute";
     }
 
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+        model.addAttribute("users", list);
+    }
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
     @Data
     static class User {
         private String username;
