e94bd3419df650fa10926eea986b60b08fa728d7
mk-isos <mkisos7900@naver.com>
2025-03-25 15:52:32 +0900

    250325 feat: apply HttpSession for login, logout, and session-based home view

--- springmvc2/login/src/main/java/hello/login/web/HomeController.java
+++ springmvc2/login/src/main/java/hello/login/web/HomeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -46,7 +47,7 @@ public class HomeController {
 
     }
 
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
 
         //세션 관리자에 저장된 회원 정보 조회
@@ -58,6 +59,24 @@ public class HomeController {
         }
 
         model.addAttribute("member", member);
+        return "loginHome";
+
+    }
+    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
         return "loginHome";
 
     }
