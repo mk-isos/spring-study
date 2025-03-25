a12c650c0db4e8955455a53dd91ea48fc0bcd0b1
mk-isos <mkisos7900@naver.com>
2025-03-25 16:02:15 +0900

    250325 feat: simplify session handling with @SessionAttribute and configure session tracking mode

--- springmvc2/login/src/main/java/hello/login/web/HomeController.java
+++ springmvc2/login/src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServletRequest;
@@ -62,6 +63,7 @@ public class HomeController {
         return "loginHome";
 
     }
+
     @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
@@ -71,6 +73,19 @@ public class HomeController {
         }
 
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
 
         if (loginMember == null) {
             return "home";
