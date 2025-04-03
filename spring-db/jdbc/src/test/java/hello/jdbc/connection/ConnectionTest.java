91d5d7e6456237d6e86692339288ff8c3ec6db9a
mk-isos <mkisos7900@naver.com>
2025-04-03 14:04:18 +0900

    250403 feat: apply DataSource using DriverManagerDataSource for connection management

--- spring-db/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ spring-db/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -30,19 +30,7 @@ public class ConnectionTest {
         useDataSource(dataSource);
     }
 
-    @Test
-    void dataSourceConnectionPool() throws SQLException, InterruptedException {
-        //커넥션 풀링
-        HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(URL);
-        dataSource.setUsername(USERNAME);
-        dataSource.setPassword(PASSWORD);
-        dataSource.setMaximumPoolSize(10);
-        dataSource.setPoolName("MyPool");
 
-        useDataSource(dataSource);
-        Thread.sleep(1000);
-    }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
         Connection con1 = dataSource.getConnection();
