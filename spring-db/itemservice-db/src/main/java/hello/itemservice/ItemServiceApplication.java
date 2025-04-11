4ba571521d70ec5a82258b30bd7512bf50a95a02
mk-isos <mkisos7900@naver.com>
2025-04-11 09:33:29 +0900

    250411 feat: integrate MyBatis repository implementation and configuration

--- spring-db/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ spring-db/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -3,6 +3,7 @@ package hello.itemservice;
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,7 +17,9 @@ import javax.sql.DataSource;
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
-@Import(JdbcTemplateV3Config.class)
+//@Import(JdbcTemplateV3Config.class)
+@Import(MyBatisConfig.class)
+@MapperScan("hello.itemservice.repository.mybatis")
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 @Slf4j
 public class ItemServiceApplication {
