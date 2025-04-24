873b1ddce0cce802a6edfffd66a5b9e7bcbad8f6
mk-isos <mkisos7900@naver.com>
2025-04-24 11:16:49 +0900

    250424 feat: apply @Order to control aspect execution sequence in AspectV5Order

--- advanced/aop/src/test/java/hello/aop/AopTest.java
+++ advanced/aop/src/test/java/hello/aop/AopTest.java
@@ -5,6 +5,7 @@ import hello.aop.order.OrderService;
 import hello.aop.order.aop.AspectV1;
 import hello.aop.order.aop.AspectV3;
 import hello.aop.order.aop.AspectV4Pointcut;
+import hello.aop.order.aop.AspectV5Order;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -18,8 +19,8 @@ import org.springframework.context.annotation.Import;
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
-@Import(AspectV4Pointcut.class)
-//@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+//@Import(AspectV4Pointcut.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 //@Import(AspectV6Advice.class)
 public class AopTest {
 
