3e4ef503adf7061171b4a7c75b915ed1e3412ad6
mk-isos <mkisos7900@naver.com>
2025-05-01 10:30:09 +0900

    250501 feat: add MemoryCondition for custom conditional bean registration

--- boot/autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ boot/autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -1,13 +1,16 @@
 package hello.config;
 
+import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 //@Conditional(MemoryCondition.class)
-//@ConditionalOnProperty(name = "memory", havingValue = "on") //추가
+@ConditionalOnProperty(name = "memory", havingValue = "on") //추가
 public class MemoryConfig {
 
     @Bean
