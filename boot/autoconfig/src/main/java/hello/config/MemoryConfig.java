cc73b47412b3a19e8fc80b02e18615635016da81
mk-isos <mkisos7900@naver.com>
2025-05-01 10:20:37 +0900

    250501 feat: add memory usage API with MemoryFinder and conditional config setup

---
+++ boot/autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -0,0 +1,23 @@
+package hello.config;
+
+import memory.MemoryController;
+import memory.MemoryFinder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@Conditional(MemoryCondition.class)
+//@ConditionalOnProperty(name = "memory", havingValue = "on") //추가
+public class MemoryConfig {
+
+    @Bean
+    public MemoryController memoryController() {
+        return new MemoryController(memoryFinder());
+    }
+
+    @Bean
+    public MemoryFinder memoryFinder() {
+        return new MemoryFinder();
+    }
+
+}
