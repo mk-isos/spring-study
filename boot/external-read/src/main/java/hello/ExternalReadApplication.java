61523e7a32977da35ef347b2d5fb303ab37f2d9c
mk-isos <mkisos7900@naver.com>
2025-05-08 17:18:59 +0900

    250508 feat: register PayClient beans based on profile using @Profile annotation

--- boot/external-read/src/main/java/hello/ExternalReadApplication.java
+++ boot/external-read/src/main/java/hello/ExternalReadApplication.java
@@ -1,5 +1,6 @@
 package hello;
 
+import hello.config.MyDataSourceConfigV3;
 import hello.config.MyDataSourceEnvConfig;
 import hello.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
@@ -7,8 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 //@Import(MyDataSourceEnvConfig.class)
-@Import(MyDataSourceValueConfig.class)
-@SpringBootApplication(scanBasePackages = {"hello.datasource"})
+//@Import(MyDataSourceValueConfig.class)
+@Import(MyDataSourceConfigV3.class)
+@SpringBootApplication(scanBasePackages = {"hello.datasource" , "hello.pay"})
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
