94cff45a1c7f72719662f5559bd424afba542522
mk-isos <mkisos7900@naver.com>
2025-06-11 09:32:27 +0900

    250611 feat: add MeterRegistry metrics tracking in OrderServiceV1 for order and cancel

--- boot/actuator/src/main/java/hello/ActuatorApplication.java
+++ boot/actuator/src/main/java/hello/ActuatorApplication.java
@@ -2,8 +2,16 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+//@Import(OrderConfigV0.class)
+//@Import(OrderConfigV1.class)
+//@Import(OrderConfigV2.class)
+//@Import(OrderConfigV3.class)
+//@Import(OrderConfigV4.class)
+//@Import({OrderConfigV4.class, StockConfigV1.class})
+//@Import({OrderConfigV4.class, StockConfigV2.class})
+@SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
     public static void main(String[] args) {
