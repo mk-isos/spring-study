86c86dac51d54b700545da18e655a71cf6a96bd5
mk-isos <mkisos7900@naver.com>
2025-06-13 13:55:31 +0900

    250613 feat: add core JPA entities and JpaMain for jpashop domain model

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import org.h2.engine.User;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+
+}
