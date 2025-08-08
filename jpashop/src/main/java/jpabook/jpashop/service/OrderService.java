d305b0be9f705748c1caee92c9e3af99937042bb
mk-isos <mkisos7900@naver.com>
2025-08-08 16:55:47 +0900

    250808 feat: add InitDb component for initial database seeding with sample members, items, and orders

--- jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -43,7 +43,7 @@ public class OrderService {
         OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
 
         //주문 생성
-        Order order = Order.creatOrder(member, delivery, orderItem);
+        Order order = Order.createOrder(member, delivery, orderItem);
 
         //주문 저장
         orderRepository.save(order);
