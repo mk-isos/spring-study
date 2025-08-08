13cc7666fe552e8953ceef1b79a495377f2106a2
mk-isos <mkisos7900@naver.com>
2025-08-08 14:18:58 +0900

    250808 feat: implement member registration API v1 without DTO

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +15,7 @@ public class Member {
     @Id @GeneratedValue
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded
