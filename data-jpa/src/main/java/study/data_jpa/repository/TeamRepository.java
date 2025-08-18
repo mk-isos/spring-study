ed05e55a6ac310b7165301accbc9b3891897aac6
mk-isos <mkisos7900@naver.com>
2025-08-18 11:17:22 +0900

    250818 refactor: update TeamRepository implementation for improved entity management

---
+++ data-jpa/src/main/java/study/data_jpa/repository/TeamRepository.java
@@ -0,0 +1,8 @@
+package study.data_jpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.data_jpa.entitiy.Team;
+
+//@Repository 어노테이션 없어도 스캔 가능
+public interface TeamRepository extends JpaRepository<Team, Long> {
+}
