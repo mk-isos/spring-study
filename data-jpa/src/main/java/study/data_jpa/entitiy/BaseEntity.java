721cba0810c92fe46c4ffe1d055b7935e0b90f9b
mk-isos <mkisos7900@naver.com>
2025-08-20 10:05:06 +0900

    250820 feat: enable Spring Data JPA auditing with BaseEntity (@CreatedDate, @LastModifiedDate, @CreatedBy, @LastModifiedBy) and AuditorAware config

---
+++ data-jpa/src/main/java/study/data_jpa/entitiy/BaseEntity.java
@@ -0,0 +1,36 @@
+package study.data_jpa.entitiy;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    //등록자 수정자
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+}
