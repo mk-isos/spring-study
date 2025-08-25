bacf161cfebad3ae0ff0eca5201c6a3ced59cdfb
mk-isos <mkisos7900@naver.com>
2025-08-25 11:19:04 +0900

    250825 test: demonstrate QMember instance usage with explicit alias and default static instance in Querydsl

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -75,4 +75,21 @@ public class QuerydslBasicTest {
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
+
+    /**
+     * **Q클래스 인스턴스를 사용하는 2가지 방법**
+     *
+     * QMember qMember = new QMember("m"); //별칭 직접 지정
+     * QMember qMember = QMember.member; //기본 인스턴스 사용
+     */
+    @Test
+    public void startQuerydsl3() {
+        //member1을 찾아라.
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
 }
