c0066c449efef86ee60b4900ece41584d1328122
mk-isos <mkisos7900@naver.com>
2025-08-28 09:14:17 +0900

    250828 feat: integrate QuerydslPredicateExecutor into MemberRepository for dynamic predicate-based queries

--- querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
@@ -1,10 +1,12 @@
 package study.querydsl.repository;
 
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Member;
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<User> {
 
     List<Member> findByUsername(String username);
 }
